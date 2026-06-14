6693356d57e776d6d1af66cadb633882b9224cb3
Vladislav Voinov <voinoff.vlad2016@yandex.ru>
2026-06-15 02:47:55 +0300

    Added search for True/Vera Crypt, LUKS, EncFS

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <fstream>
 #include <filesystem>
 #include <entropy/shannon_entropy.h>
 
 namespace fs = std::filesystem;
 
 void usage_exit(){
-    std::cout<<"Crypto search work only with TrueCrypt/VeraCrypt, EncFS, LUKS, PGP containers.\nUsage:\n\tcrypto_search --folder 'Folder'\n\nYou can use next commands:\n\t--help - to see this message\n\t--version - to see version of the program\n\t--folder - to set folder to search in\n\t--recursive - to check nested folders\n";
+    std::cout<<"Crypto search work only with TrueCrypt/VeraCrypt, EncFS, LUKS, PGP containers.\n\
+                Usage:\n\
+                \tcrypto_search --folder 'Folder'\n\n\
+                You can use next commands:\n\
+                \t--help - to see this message\n\
+                \t--version - to see version of the program\n\
+                \t--folder - to set folder to search in\n\
+                \t--recursive - to check nested folders\n";
     exit(EXIT_SUCCESS);
 }
 
@@ -17,6 +25,7 @@ void version_exit(){
 }
 
 int main(int argc, char** argv) {
+    setlocale(LC_ALL, 0);
     fs::path searching_folder{"/"}; 
     bool is_recursive = false;
 
@@ -41,8 +50,31 @@ int main(int argc, char** argv) {
         {
             std::cout<<entry<<std::endl;
             if(entry.is_directory() == false){
+                // check for LUKSs header
+                std::ifstream byte_stream(entry.path(), std::ios::binary);
+                char some_bytes[5]{};
+                byte_stream.read(reinterpret_cast<char*>(&some_bytes), sizeof some_bytes);
+                some_bytes[4] = '\0';
+                if(strcmp(some_bytes, "LUKS") == 0){
+                    std::cout << "File is encrypted with LUKS" << std::endl;
+                    continue;
+                }
+                
+                // check for TrueCrypt/VeraCrypt
                 double entropy_value = checker.get_file_entropy(entry.path());
-                std::cout << "Shannon Entropy of '" << entry << "': " << entropy_value << " (out of 8.0)\n";
+                if(entropy_value > 7.9 && entry.file_size()%512 == 0){
+                    std::cout << "File is encrypted with TrueCrypt\\VeraCrypt" << std::endl;
+                    continue;
+                }
+            }
+            else{
+                for(const auto& nested_entry : fs::directory_iterator(entry)){
+                    std::string filename{nested_entry.path().stem().string()};
+                    if(filename.compare(".encfs6") == 0){
+                        std::cout << "This folder encrypted with EncFS" << std::endl;
+                        break;
+                    }
+                }
             }
         }
     } 
